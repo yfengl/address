495d481b35d9f3b915c72e3fb53686c179792646
yfengleng <yfengleng@gmail.com>
2020-12-19 13:12:17 +0800

    修改数据库文件和REDAME

---
+++ sql/database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS classlist DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+CREATE USER IF NOT EXISTS 'dbuser'@'localhost' IDENTIFIED BY '123456';
+
+GRANT ALL ON classlist .* TO 'dbuser'@'localhost';
